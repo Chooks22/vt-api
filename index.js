f2de21a17833b025a450e2ede6fdaace19527669
Choooks22 <49726759+Choooks22@users.noreply.github.com>
2020-08-02 00:29:37 +0800

    run api only on startup, add helmet and cors

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 require('dotenv').config();
-require('./apps/api');
+require('./apps/api').main();
+
+const { logger } = require('./modules');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const { logger } = require('./modules');
+const helmet = require('helmet');
+const cors = require('cors');
 
 const PORT = +process.env.PORT || 2434;
 const app = express();
+
+app.use(helmet());
+app.use(cors());
+
+app.use(require('./apps/routes'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
