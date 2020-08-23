e4629d25f780fa3f3fa4ecfcff016c4dcf05b2ba
Brian Hsieh <yunganhsieh.cs04@nctu.edu.tw>
2020-08-23 15:44:54 +0000

    Update entrypoint.sh to copy channel json files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,34 @@
 #!/bin/bash
+LONGOPTS=(animare,hanayori,hololive,honeystrap,nijisanji,noripro,others,react,sugarlyric,vapart,vivid)
+PROG=`basename $0`
+PARSED=`getopt -o h -l $LONGOPTS -n $PROG -- "$@"`
+[ $? -ne 0 ] && { echo; usage_and_exit; }
+eval set -- "${PARSED}"
+
+usage_and_exit() {
+    cat << EOF
+Usage: entrypoint.sh
+description: Copy group json files
+EOF
+    exit 1
+}
+
+while true; do
+    case "$1" in
+        --animare|--hanayori|--hololive|--honeystrap|--nijisanji|--noripro|--others|--react|--sugarlyric|--vapart|--vivid)
+            cp -v channels/default/${1#*--}.json channels/
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+        -h|*)
+            usage_and_exit
+            exit 1
+            ;;
+    esac
+done
 
 until channelsCount=`mongo vtapi-mongo/data --quiet --eval="db.channels.count()"`; do
     >&2 echo "Mongo is unavailable - sleeping"
