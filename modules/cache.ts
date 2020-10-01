f5446c65221dcfb8df480da768b8de4ee1838410
Choooks22 <49726759+Choooks22@users.noreply.github.com>
2020-10-01 14:00:41 +0800

    Reverted to use memcached module but with promisified functions
    commit 48596c25171049194c4d9d42bc752d360fa1c512
    Author: Choooks22 <49726759+Choooks22@users.noreply.github.com>
    Date:   Thu Oct 1 13:49:04 2020 +0800
    
        write own promisified functions
    
    commit 8cb91c24efee19c86782b57a5aff57a5041be8a9
    Author: Choooks22 <49726759+Choooks22@users.noreply.github.com>
    Date:   Thu Oct 1 13:48:51 2020 +0800
    
        moved to memcached

--- modules/cache.ts
+++ modules/cache.ts
@@ -1,26 +1,63 @@
-import Memcached from 'memcached-promise';
-const re = /(?<=_).*/;
+import memcached from 'memcached';
 
-const memcached = new Memcached(
-  (process.env.MEMCACHED_HOST ?? 'localhost') + ':11211', {
-    timeout: 1000,
-    retries: 1
-  }
-);
+const { MEMCACHED_HOST, MEMCACHED_PORT } = process.env;
+const URI = `${MEMCACHED_HOST}:${MEMCACHED_PORT}`;
+const OPTIONS = {
+  timeout: 1000,
+  retries: 1,
+  namespace: 'vt'
+};
 
-export function get(key) {
-  return memcached.get(key);
-}
-export function save(key, data, ttl = 0) {
-  memcached.set(key, data, ttl);
-}
-export async function getMultiVideos(keys) {
-  const multiCache = await memcached.getMulti(keys);
-  return Object.assign({}, ...Object
-    .entries(multiCache)
-    .map(removeGroup));
+const cache = new memcached(URI, OPTIONS);
+
+/**
+ * Stores a new key-value pair in memory.
+ * @param key Key string.
+ * @param value Value to store.
+ * @param ttl TTL in seconds.
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function set(key: string, value: any, ttl = 0) {
+  return new Promise((res, rej) => {
+    cache.set(key, value, ttl, (err, data) => {
+      if (err) rej(err);
+      res(data);
+    });
+  });
 }
 
-function removeGroup([key, value]) {
-  return { [`${key.match(re)}`]: value };
+/** Get Memcached's status */
+export function stats() {
+  return new Promise((res, rej) => {
+    cache.stats((err, data) => {
+      if (err) rej(err);
+      res(data);
+    });
+  });
+}
+
+/**
+ * Retrieves one key-value pair from memory.
+ * @param key Key to retrieve from memory.
+ */
+export function get(key: string) {
+  return new Promise((res, rej) => {
+    cache.get(key, (err, data) => {
+      if (err) rej(err);
+      res(data);
+    });
+  });
+}
+
+/**
+ * Retrieves multiple key-value pairs from memory.
+ * @param keys Keys to retrieve from memory.
+ */
+export function gets(...keys: string[]): Promise<{[key: string]: unknown;}> {
+  return new Promise((res, rej) => {
+    cache.getMulti(keys.flat(), (err, data) => {
+      if (err) rej(err);
+      res(data);
+    });
+  });
 }
