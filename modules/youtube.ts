3f9ca35981153e0e7b41253b29eed03e18be6bdf
Choooks22 <49726759+Choooks22@users.noreply.github.com>
2020-10-01 16:43:59 +0800

    youtube: rename stuff and properly throw errors from youtube

--- modules/youtube.ts
+++ modules/youtube.ts
@@ -1,25 +1,25 @@
-import node_fetch from 'node-fetch';
+import fetch from 'node-fetch';
 import { SearchParams, YoutubeResponse } from './types/youtube';
 
 const URL = 'https://www.googleapis.com/youtube/v3/';
 const SETTINGS = `key=${process.env.GOOGLE_API_KEY}&accept=application/json&`;
 
-async function fetch(type: string, params: SearchParams): Promise<YoutubeResponse> {
-  return node_fetch(`${URL}${type}?${parseParams(params)}`)
+const api = (type: string, params: SearchParams): Promise<YoutubeResponse> =>
+  fetch(`${URL}${type}?${parseParams(params)}`)
+    .then(res => res.json())
     .then(res => {
-      if (res.ok) return res.json();
-      throw new Error(`${res.statusText}, status code: ${res.status}`);
+      if (res.error) throw res.error;
+      return res;
     });
-}
 
 export function videos(params: SearchParams) {
-  return fetch('videos', params);
+  return api('videos', params);
 }
 export function channels(params: SearchParams) {
-  return fetch('channels', params);
+  return api('channels', params);
 }
 export function playlistItems(params: SearchParams) {
-  return fetch('playlistItems', params);
+  return api('playlistItems', params);
 }
 
 function parseParams(params: SearchParams = <SearchParams>{}) {
