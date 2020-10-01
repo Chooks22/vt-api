c237d5f7ba66da02d7b03c6aa9a24a690e63149c
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2020-10-01 14:31:26 +0800

    Moved modules to typescript (#7)
    * Updated youtube module
    
    commit 67d4a8c246920ea980eba79448c30609a573568b
    Author: Choooks22 <49726759+Choooks22@users.noreply.github.com>
    Date:   Wed Sep 30 22:52:55 2020 +0800
    
        migrate to typescript
    
    commit 9cd35749ea839998e31f29943f8ae9c24f515958
    Author: Choooks22 <49726759+Choooks22@users.noreply.github.com>
    Date:   Wed Sep 30 22:52:30 2020 +0800
    
        add types
    
    * Reverted to use memcached module but with promisified functions
    
    commit 48596c25171049194c4d9d42bc752d360fa1c512
    Author: Choooks22 <49726759+Choooks22@users.noreply.github.com>
    Date:   Thu Oct 1 13:49:04 2020 +0800
    
        write own promisified functions
    
    commit 8cb91c24efee19c86782b57a5aff57a5041be8a9
    Author: Choooks22 <49726759+Choooks22@users.noreply.github.com>
    Date:   Thu Oct 1 13:48:51 2020 +0800
    
        moved to memcached
    
    * Added missing & in params

---
+++ modules/types/youtube.ts
@@ -0,0 +1,93 @@
+export interface SearchParams {
+  /**
+   * Available parts:
+   * - contentDetails
+   * - fileDetails
+   * - id
+   * - liveStreamingDetails
+   * - localizations
+   * - player
+   * - processingDetails
+   * - recordingDetails
+   * - snippet
+   * - statistics
+   * - status
+   * - suggestions
+   * - topicDetails
+   */
+  part: string;
+  fields?: string;
+  id?: string;
+  hl?: string;
+}
+
+type DateString = string;
+type ChannelId = string;
+
+export interface Snippet {
+  publishedAt?: DateString;
+  channelId?: ChannelId;
+  title?: string;
+  description?: string;
+  thumbnails?: {
+    [key: string]: {
+      url?: string;
+      width?: number;
+      height?: number;
+    };
+  };
+  channelTitle?: string;
+  tags?: string[];
+  categoryId?: string;
+  liveBroadcastContent?: string;
+  defaultLanguage?: string;
+  localized?: {
+    title?: string;
+    description?: string;
+  };
+  defaultAudioLanguage?: string;
+}
+
+export interface Statistics {
+  viewCount?: number;
+  likeCount?: number;
+  dislikeCount?: number;
+  favoriteCount?: number;
+  commentCount?: number;
+  commentcount?: number;
+}
+
+export interface LiveStreamingDetails {
+  actualStartTime?: DateString;
+  actualEndTime?: DateString;
+  scheduledStartTime?: DateString;
+  scheduledEndTime?: DateString;
+  concurrentViewers?: number;
+  activeLiveChatId?: number;
+}
+
+export interface YoutubeVideoObject {
+  snippet?: Snippet;
+  contentDetails?: Record<string, unknown>;
+  status?: Record<string, unknown>;
+  statistics?: Statistics;
+  player?: Record<string, unknown>;
+  topicDetails?: Record<string, unknown>;
+  recordingDetails?: Record<string, unknown>;
+  fileDetails?: Record<string, unknown>;
+  suggestions?: Record<string, unknown>;
+  liveStreamingDetails?: LiveStreamingDetails;
+  localizations?: Record<string, unknown>;
+}
+
+export interface YoutubeResponse {
+  kind: string;
+  etag: string;
+  nextPageToken?: string;
+  prevPageToken?: string;
+  pageInfo?: {
+    totalResults?: number;
+    resultsPerPage?: number;
+  };
+  items: YoutubeVideoObject[];
+}
