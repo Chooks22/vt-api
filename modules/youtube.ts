0fa18c409722279059c4277070173b128e323a86
Choooks22 <49726759+Choooks22@users.noreply.github.com>
2020-10-26 18:22:45 +0800

    youtube: update types and format

--- modules/youtube.ts
+++ modules/youtube.ts
@@ -1,29 +1,12 @@
 import fetch from 'node-fetch';
+import { SearchParams, YoutubeVideoResponse, PlaylistParams, YoutubeChannelResponse, YoutubePlaylistItemsResponse } from './types/youtube';
 
 const URL = 'https://www.googleapis.com/youtube/v3/';
-const SETTINGS = `key=${process.env.GOOGLE_API_KEY}&accept=application/json&`;
+const SETTINGS = `?key=${process.env.GOOGLE_API_KEY}&accept=application/json&`;
 
-async function api(type: string, params: SearchParams): Promise<YoutubeResponse> {
-  return fetch(`${URL}${type}?${parseParams(params)}`)
-    .then(res => res.json())
-    .then(res => {
-      if (res.error) throw res.error;
-      return res;
-    });
-}
+const parseParams = (params = <SearchParams>{}) => SETTINGS + Object.entries(params).map(([k, v]) => k + '=' + v).join('&');
+const youtubeFetch = async (type: string, params: SearchParams) => fetch(`${URL}${type}?${parseParams(params)}`).then(res => res.json()).then(res => { if (res.error) throw res.error; return res; });
 
-export function videos(params: SearchParams) {
-  return api('videos', params);
-}
-export function channels(params: SearchParams) {
-  return api('channels', params);
-}
-export function playlistItems(params: PlaylistParams) {
-  return api('playlistItems', params);
-}
-
-function parseParams(params: SearchParams = <SearchParams>{}) {
-  return SETTINGS + Object.entries(params)
-    .map(([k, v]) => k + '=' + v)
-    .join('&');
-}
+export const videos = (params: SearchParams): Promise<YoutubeVideoResponse> => youtubeFetch('videos', params);
+export const channels = (params: SearchParams): Promise<YoutubeChannelResponse> => youtubeFetch('channels', params);
+export const playlistItems = (params: PlaylistParams): Promise<YoutubePlaylistItemsResponse> => youtubeFetch('playlistItems', params);
