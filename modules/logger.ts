f82b8a7ce5d426b452ab305d47d582eaa8006e42
Choooks22 <49726759+Choooks22@users.noreply.github.com>
2020-10-30 22:58:46 +0800

    add log levels

--- modules/logger.ts
+++ modules/logger.ts
@@ -1,4 +1,10 @@
 import { debug as Debug } from 'debug';
+/* eslint-disable indent,no-multi-spaces */
+const LOG_LEVEL = !process.env.LOG_LEVEL     ? 2
+                : +process.env.LOG_LEVEL < 0 ? 0
+                : +process.env.LOG_LEVEL > 3 ? 3
+                : +process.env.LOG_LEVEL;
+/* eslint-enable indent,no-multi-spaces */
 export default function debug(namespace: string) {
   const logger = Debug(namespace);
   const log = logger.extend('[ LOG ]:');
@@ -12,6 +18,11 @@ export default function debug(namespace: string) {
   const error = logger.extend('[ ERROR ]:');
   error.log = console.error.bind(console);
   error.color = '196';
+  switch (LOG_LEVEL) {
+  case 0: warn.enabled = false;
+  case 1: info.enabled = false;
+  case 2: log.enabled = false;
+  }
   return { extend: extend.bind(logger), log, info, warn, error };
 }
 function extend(this: debug.Debugger, namespace: string) {
