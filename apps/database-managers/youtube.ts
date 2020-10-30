d317eb92b3832f8acfacc399c50a085bcf3eaada
Choooks22 <49726759+Choooks22@users.noreply.github.com>
2020-10-30 23:04:28 +0800

    add database manager for youtube api

---
+++ apps/database-managers/youtube.ts
@@ -0,0 +1,63 @@
+import { EventEmitter } from 'events';
+import { YoutubeChannelData, YoutubeVideoObject } from '../apis/youtube/types';
+import { debug } from '../../modules';
+import * as db from '../../database';
+
+const logger = debug('apis:youtube:database-manager');
+
+interface DatabaseEvents {
+  'save-videos': (newVideos: YoutubeVideoObject[]) => void;
+  'update-videos': (videos: YoutubeVideoObject[]) => void;
+  'update-channels': (channels: YoutubeChannelData[]) => void;
+}
+
+declare interface DatabaseManager {
+  on<U extends keyof DatabaseEvents>(
+    event: U, listener: DatabaseEvents[U]
+  ): this;
+  emit<U extends keyof DatabaseEvents>(
+    event: U, ...args: Parameters<DatabaseEvents[U]>
+  ): boolean;
+}
+
+class DatabaseManager extends EventEmitter { constructor() { super(); } }
+const database = new DatabaseManager();
+export default database;
+
+database.on('save-videos', async newVideos => {
+  logger.log(`Saving ${newVideos.length} videos...`);
+  const results = await Promise.all(newVideos
+    .map(video => db.Videos.updateOne(
+      { channel_id: video.channel_id },
+      { $setOnInsert: video },
+      { upsert: true }
+    ))).then(writeResults => writeResults.reduce(
+    (total, result) => total + (result.upserted?.length ?? 0), 0
+  )).catch(logger.error);
+  if (!isNaN(results)) logger.log(`Finished saving ${results} videos.`);
+});
+
+database.on('update-videos', async videos => {
+  logger.log(`Updating ${videos.length} videos...`);
+  const results = await Promise.all(videos
+    .map(video => db.Videos.updateOne(
+      { channel_id: video.channel_id },
+      { $set: video }
+    ))).then(writeResults => writeResults.reduce(
+    (total, result) => total + result.nModified, 0)
+  ).catch(logger.error);
+  if (results) logger.log(`Updated ${results} videos.`);
+});
+
+database.on('update-channels', async channels => {
+  logger.log(`Updating ${channels.length} youtube channels...`);
+  const results = await Promise.all(channels
+    .map(channel => db.Channels.updateOne(
+      { channel_id: channel.channel_id },
+      { $set: channel }
+    ))
+  ).then(writeResults => writeResults.reduce(
+    (total, result) => total + result.nModified, 0
+  )).catch(logger.error);
+  if (!isNaN(results)) logger.log(`Updated ${results} channels.`);
+});
