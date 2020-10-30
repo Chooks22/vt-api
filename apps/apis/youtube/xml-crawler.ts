e77feab77c10db22470ba6b1b39470a452b955e3
Choooks22 <49726759+Choooks22@users.noreply.github.com>
2020-10-30 23:04:52 +0800

    add youtube xml crawler

--- apps/apis/youtube/xml-crawler.ts
+++ apps/apis/youtube/xml-crawler.ts
@@ -9,36 +9,63 @@ import { ChannelId } from '../../../modules/types/youtube';
 import database from '../../database-managers/youtube';
 import { VideoXmlEntry, YoutubeVideoObject } from './types';
 
-const channel = 'UC5CwaMl1eIgY8h02uZw7u8A';
-const getXmlLink = (channelId: ChannelId) => `https://www.youtube.com/feeds/videos.xml?channel_id=${channelId}&t=${Date.now()}`;
-const fetchXml = (channelId: ChannelId) => fetch(getXmlLink(channelId)).then(res => res.text());
+const logger = debug('api:youtube:xml-crawler');
+const CACHE_TTL = +process.env.TTL_LONG || 900;
 
-function parseXml(xml: string): Promise<VideoObject[]> {
-  return new Promise((res, rej) => {
-    parseString(xml, { explicitArray: false }, (err, result) => {
-      if (err) rej (err);
-      res(result?.feed?.entry?.map(parseEntries));
+export default (timer = '1 * * * * *') => Channels
+  .find({ platform_id: 'yt' })
+  .then(channelList => {
+    channelList.forEach(channel => {
+      const { channel_id, organization } = channel;
+      const xmlScraper = new XmlScraper(channel_id, organization);
+      schedule.scheduleJob(`xml-crawler:${channel_id}`, timer, crawler.bind(xmlScraper));
     });
+    logger.info(`Now scraping ${channelList.length} youtube channel xmls.`);
   });
+
+class XmlScraper {
+  private rawXmlData = null;
+  private xmlOptions = { explicitArray: false };
+  constructor(private channel_id: ChannelId, private organization: string) {}
+  private get xmlLink() { return `https://www.youtube.com/feeds/videos.xml?channel_id=${this.channel_id}&t=${Date.now()}`; }
+  get channelId() { return this.channel_id; }
+  get cacheId() { return `yt-${this.channel_id}`; }
+  async fetchXml(): Promise<YoutubeVideoObject[]> {
+    this.rawXmlData = await fetch(this.xmlLink).then(res => res.text());
+    return this.parseXml().then(this.sortVideos);
+  }
+  private parseXml(): Promise<YoutubeVideoObject[]> {
+    const parseEntries = this.parseEntries.bind(this);
+    return new Promise((res, rej) => parseString(
+      this.rawXmlData,
+      this.xmlOptions,
+      (err, result) => err ? rej(err) : res(result?.feed?.entry?.map ? result.feed.entry.map(parseEntries) : [])
+    ));
+  }
+  private sortVideos(parsedXml: YoutubeVideoObject[]) {
+    return parsedXml.sort((video1, video2) => video2.updated_at - video1.updated_at);
+  }
+  private parseEntries(entry: VideoXmlEntry): YoutubeVideoObject {
+    return {
+      _id: entry['yt:videoId'],
+      platform_id: 'yt',
+      channel_id: entry['yt:channelId'],
+      organization: this.organization,
+      title: entry.title,
+      status: 'new',
+      updated_at: +new Date(entry.updated)
+    };
+  }
 }
 
-const parseEntries = (entry: VideoEntry) => ({
-  videoId: entry['yt:videoId'],
-  channel: entry['yt:channelId'],
-  title: entry.title,
-  timestamp: +new Date(entry.published)
-});
-
-fetchXml(channel)
-  .then(xml => parseXml(xml))
-  .then(async data => {
-    if (!data) return console.error(`feed from channel ${channel} returned empty?`);
-    const latestTimestamp = (await memcache.get(`yt-${channel}`)) ?? 0;
-    const newVideos = data
-      .filter(item => item.timestamp > latestTimestamp)
-      .sort((item1, item2) => item2.timestamp - item1.timestamp);
-    if (!newVideos.length) return;
-    memcache.set(`yt-${channel}`, newVideos[0].timestamp, 900);
-    console.log(newVideos);
-  })
-  .catch(error => console.error(error));
+async function crawler(this: XmlScraper) {
+  logger.log(`Crawling ${this.channelId}...`);
+  const latestTimestamp = (await memcache.get(this.cacheId)) ?? 0;
+  const videoList = await this.fetchXml();
+  if (!videoList.length) return logger.warn(`${this.channelId} didn\'t return anything?`);
+  const newVideos = videoList.filter(video => video.updated_at > latestTimestamp);
+  if (!newVideos.length) return logger.log(`${this.channelId} doesn\'t have new videos.`);
+  logger.info(`Found ${newVideos.length} new videos from ${this.channelId}`);
+  memcache.set(this.cacheId, newVideos[0].updated_at, CACHE_TTL);
+  database.emit('save-videos', newVideos);
+}
