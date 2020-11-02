ef6052e242bddc97d74d96fffcbb70239e244c97
Choooks22 <49726759+Choooks22@users.noreply.github.com>
2020-11-02 17:12:01 +0800

    set updated_at on new documents on pre save video middleware

--- database/middlewares/VideoMiddleware.ts
+++ database/middlewares/VideoMiddleware.ts
@@ -4,3 +4,7 @@ import { VideoProps } from '../types/videos';
 VideoSchema.pre<VideoProps>('updateOne', function() {
   this.set({ updated_at: Date.now() });
 });
+
+VideoSchema.pre<VideoProps>('save', async function() {
+  if (this.isNew) this.updated_at = Date.now();
+});
