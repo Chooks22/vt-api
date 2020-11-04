20c483948fd3fc6e47fe5ad0af843d4ffb9bc771
Choooks22 <49726759+Choooks22@users.noreply.github.com>
2020-11-04 20:53:07 +0800

    move modules to src/modules

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,4 +1,4 @@
-export * from '../database';
+export * from '../../database';
 export * as memcache from './cache';
 export { default as debug } from './logger';
 export * as youtube from './youtube';
