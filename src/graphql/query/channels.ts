f258925b2d1ec7cb7828c2f3cb1d57bad9b6ae99
Choooks22 <49726759+Choooks22@users.noreply.github.com>
2020-11-07 23:43:41 +0800

    add cursor pagination

--- src/graphql/query/channels.ts
+++ src/graphql/query/channels.ts
@@ -2,7 +2,7 @@ import { ApolloError, UserInputError } from 'apollo-server';
 import { PlatformId } from '../../../database/types/members';
 import { Channels, memcache } from '../../modules';
 import { ChannelId } from '../../modules/types/youtube';
-import { cutChannelIds, cutGroupString, firstField, getCacheKey, parseOrganization, Sort } from './consts';
+import { cutChannelIds, cutGroupString, firstField, getCacheKey, getNextToken, parseOrganization, parseToken, Sort } from './consts';
 
 const CACHE_TTL = +(process.env.TTL_LONG ?? 900);
 
@@ -10,7 +10,6 @@ interface OrderBy {
   _id?: Sort;
   published_at?: Sort;
   subscribers?: Sort;
-  videos?: Sort;
 }
 
 interface ChannelsQuery {
@@ -20,35 +19,59 @@ interface ChannelsQuery {
   platforms: PlatformId[];
   channel_id: ChannelId[];
   order_by: OrderBy;
+  next_page_token: string;
   limit: number;
 }
 
 export async function channels(_, query: ChannelsQuery) {
   try {
-    const { _id = [], name = '', organizations = [], channel_id = [], platforms = [], limit } = query;
+    const {
+      _id = [],
+      name = '',
+      organizations = [],
+      channel_id = [],
+      platforms = [],
+      next_page_token = '',
+      limit
+    } = query;
     if (limit < 1 || limit > 50) {
       return new UserInputError('limit must be between 1-50 inclusive.');
     }
     const [ORDER_BY, ORDER_BY_KEY] = firstField(query.order_by);
+    const [ORDER_KEY, ORDER_VALUE] = Object.entries(ORDER_BY)[0];
+    const sortById = ORDER_KEY === '_id';
+    const sortBy = sortById ? ORDER_BY : { [`channel_stats.${ORDER_KEY}`]: ORDER_VALUE };
     const ORGANIZATIONS = parseOrganization(organizations);
-    const CACHE_KEY = getCacheKey(`CHNLS:${_id}${(name)}${cutGroupString(ORGANIZATIONS)}${cutChannelIds(channel_id)}${platforms}${ORDER_BY_KEY}`, false);
+    const CACHE_KEY = getCacheKey(`CHNLS:${_id}${(name)}${cutGroupString(ORGANIZATIONS)}${cutChannelIds(channel_id)}${platforms}${limit}${ORDER_BY_KEY}${next_page_token}`, false);
 
     const cached = await memcache.get(CACHE_KEY);
     if (cached) return cached;
 
     const uncachedChannels = await Channels.find({
       _id: { [_id[0] ? '$in' : '$nin']: _id },
+      ...next_page_token && { [Object.keys(sortBy)[0]]: { [ORDER_VALUE === 'asc' ? '$gte' : '$lte']: parseToken(next_page_token) } },
       ...name && { $or: getNameQueries(name) },
       ...ORGANIZATIONS[0] && { organization: { $in: ORGANIZATIONS } },
       ...channel_id[0] && { channel_id: { $in: channel_id } },
       ...platforms[0] && { platform_id: { $in: platforms } }
-    }).sort(ORDER_BY)
-      .limit(limit)
+    }).sort(sortBy)
+      .limit(limit + 1)
       .lean()
       .exec();
 
-    memcache.set(CACHE_KEY, uncachedChannels, CACHE_TTL);
-    return uncachedChannels;
+    const results = {
+      items: uncachedChannels,
+      next_page_token: null
+    };
+
+    const hasNextPage = uncachedChannels.length > limit && results.items.pop();
+    if (hasNextPage) {
+      const token = sortById ? hasNextPage._id : hasNextPage.channel_stats[ORDER_KEY];
+      results.next_page_token = getNextToken(token);
+    }
+
+    memcache.set(CACHE_KEY, results, CACHE_TTL);
+    return results;
   } catch(err) {
     throw new ApolloError(err);
   }
--- src/graphql/query/videos.ts
+++ src/graphql/query/videos.ts
@@ -3,7 +3,7 @@ import { PlatformId } from '../../../database/types/members';
 import { memcache, Videos } from '../../modules';
 import { ChannelId } from '../../modules/types/youtube';
 import { VideoStatus } from '../../server/apis/youtube/types';
-import { firstField, getCacheKey, parseOrganization, Sort } from './consts';
+import { firstField, getCacheKey, getNextToken, parseOrganization, parseToken, Sort } from './consts';
 
 interface SortBy {
   published?: Sort;
@@ -18,39 +18,61 @@ interface VideoQuery {
   platforms: PlatformId[];
   max_upcoming_mins: number;
   order_by: SortBy;
+  next_page_token: string;
   limit: number;
 }
 
 export async function videos(_, query: VideoQuery) {
   try {
-    const { channel_id = [], status = [], organizations = [], platforms = [], max_upcoming_mins, limit } = query;
+    const {
+      channel_id = [],
+      status = [],
+      organizations = [],
+      platforms = [],
+      max_upcoming_mins,
+      next_page_token = '',
+      limit
+    } = query;
 
-    if (limit <= 1 || limit > 50) {
-      return new UserInputError('limit must be between 1-100 inclusive.');
+    if (limit < 1 || limit > 50) {
+      return new UserInputError('limit must be between 1-50 inclusive.');
     }
     if (max_upcoming_mins < 0 || max_upcoming_mins > 480) {
       return new UserInputError('max_upcoming_mins must be between 0-480 inclusive.');
     }
     const ORGANIZATIONS = parseOrganization(organizations);
     const [ORDER_BY, ORDER_BY_KEY] = firstField(query.order_by);
-    const CACHE_KEY = getCacheKey(`VIDS:${channel_id}${status}${organizations}${platforms}${max_upcoming_mins}${ORDER_BY_KEY}${limit}`);
+    const [ORDER_KEY, ORDER_VALUE] = Object.entries(ORDER_BY)[0];
+    const orderBy = { [`time.${ORDER_KEY}`]: ORDER_VALUE };
+    const CACHE_KEY = getCacheKey(`VIDS:${channel_id}${status}${organizations}${platforms}${max_upcoming_mins}${ORDER_BY_KEY}${limit}${next_page_token}`);
 
     const cached = await memcache.get(CACHE_KEY);
     if (cached) return cached;
 
-    const [ORDER_KEY, ORDER_VALUE] = Object.entries(ORDER_BY);
     const uncachedVideos = await Videos.find({
       status: status[0] ? { $in: status } : { $ne: 'missing' },
+      ...next_page_token && { [Object.keys(orderBy)[0]]: { [ORDER_VALUE === 'asc' ? '$gte' : '$lte']: parseToken(next_page_token) } },
       ...channel_id[0] && { channel_id: { $in: channel_id } },
       ...ORGANIZATIONS[0] && { organization: { $in: ORGANIZATIONS } },
       ...platforms[0] && { platform_id: { $in: platforms } },
       ...max_upcoming_mins && { 'time.scheduled': { $lte: Date.now() + max_upcoming_mins } }
-    }).sort({ [`time.${ORDER_KEY}`]: ORDER_VALUE })
-      .limit(limit)
+    }).sort(orderBy)
+      .limit(limit + 1)
       .exec();
 
-    memcache.set(CACHE_KEY, uncachedVideos, 60);
-    return uncachedVideos;
+    const results = {
+      items: uncachedVideos,
+      next_page_token: null
+    };
+
+    const hasNextPage = uncachedVideos.length > limit && results.items.pop();
+    if (hasNextPage) {
+      const token = hasNextPage.time[ORDER_KEY];
+      results.next_page_token = getNextToken(token);
+    }
+
+    memcache.set(CACHE_KEY, results, 60);
+    return results;
   } catch(err) {
     throw new ApolloError(err);
   }
