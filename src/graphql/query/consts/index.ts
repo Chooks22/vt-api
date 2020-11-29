19e5bae9dd247429509bdb0e5f6da92fe9d5bdba
Choooks22 <49726759+Choooks22@users.noreply.github.com>
2020-11-29 18:20:14 +0800

    Escape organization string input

--- src/graphql/query/consts/index.ts
+++ src/graphql/query/consts/index.ts
@@ -3,9 +3,9 @@ export const escapeRegex = (text: string) => text.replace(/[-\/\\^$*+?.()|[\]{}]
 export const getNextToken = (text: any) => Buffer.from(text.toString()).toString('base64');
 export const parseToken = (token: string) => Buffer.from(token, 'base64');
 export const getCacheKey = (query: string, invalidate = true) => `${query}:${invalidate ? process.env.CACHE_MINUTE : 0}`.replace(/ |\n/g, '');
-export const parseOrganization = (organizations: string[]) => organizations.sort().join('|');
+export const parseOrganization = (organizations: string[]) => escapeRegex(organizations.sort().join('|'));
 export const cutChannelIds = (idList: string[]) => idList.map(id => id.slice(0, 3) + id.slice(-3));
-export const cutGroupString = (groupList: string) => groupList.split('|').map(group => group.slice(0, 2) + group.slice(-2));
+export const cutGroupString = (groupList: string) => groupList.split('|^').map(group => group.slice(1, 3) + group.slice(-2));
 export const minsToMs = (mins: number) => mins * 6e4;
 export const firstField = (obj: {[key: string]: any;}): [{[key: string]: any;}, string] => {
   const [key, value] = Object.entries(obj)[0];
