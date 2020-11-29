a7e7a13266b829d1894cb295b6cab69aa060458f
Choooks22 <49726759+Choooks22@users.noreply.github.com>
2020-11-29 18:29:40 +0800

    Update parsing orgs to ignore empty query

--- src/graphql/query/consts/index.ts
+++ src/graphql/query/consts/index.ts
@@ -3,7 +3,7 @@ export const escapeRegex = (text: string) => text.replace(/[-\/\\^$*+?.()|[\]{}]
 export const getNextToken = (text: any) => Buffer.from(text.toString()).toString('base64');
 export const parseToken = (token: string) => Buffer.from(token, 'base64');
 export const getCacheKey = (query: string, invalidate = true) => `${query}:${invalidate ? process.env.CACHE_MINUTE : 0}`.replace(/ |\n/g, '');
-export const parseOrganization = (organizations: string[]) => '^' + escapeRegex(organizations.sort().join('|^'));
+export const parseOrganization = (organizations: string[]) => organizations.length ? '^' + escapeRegex(organizations.sort().join('|^')) : '';
 export const cutChannelIds = (idList: string[]) => idList.map(id => id.slice(0, 3) + id.slice(-3));
 export const cutGroupString = (groupList: string) => groupList.split('|').map(group => group.slice(1, 3) + group.slice(-2));
 export const minsToMs = (mins: number) => mins * 6e4;
